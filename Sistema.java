ea6a8c25dd54a203e2dc29b35f3d66c0bbcf152c
Nagel <130867271+Nagel-P@users.noreply.github.com>
2024-04-16 21:03:42 -0300

    Update Sistema.java

--- Sistema.java
+++ Sistema.java
@@ -1,6 +1,35 @@
 class Sistema {
 
     public static void executar() {
-        
+        int op;
+        do {
+            menu();
+            op = Console.lerInt();
+
+            switch (op) {
+                case 1:
+                    cadastrar();
+                    break;
+                case 2:
+                    buscar();
+                    break;
+                case 3:
+                    editar();
+                    break;
+                case 4:
+                    excluir();
+                    break;
+                case 5:
+                    listar();
+                    break;
+                case 6:
+                    excluirTodos();
+                    break;
+                case 0:
+                    System.out.println("Saindo...");
+                    break;
+                default:
+                    System.out.println("Opção inválida.");
+            }
+        } while (op != 0);
     }
-}

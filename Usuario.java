ee658de63cbb86b6e60e083f2b08eb6314be590b
Nagel <130867271+Nagel-P@users.noreply.github.com>
2024-04-17 08:11:51 -0300

    Update Usuario.java

--- Usuario.java
+++ Usuario.java
@@ -2,4 +2,48 @@ import java.util.ArrayList;
 
 abstract class Usuario {
 
+    private static ArrayList<Usuario> listaUsuarios = new ArrayList<>();
+
+    public static ArrayList<Usuario> getListaUsers() {
+        return listaUsuarios;
+    }
+
+    private String nome;
+    private String email;
+    private String profissao;
+
+    public Usuario(String nome, String email, String profissao) {
+        this.nome = nome;
+        this.email = email;
+        this.profissao = profissao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getProfissao() {
+        return profissao;
+    }
+
+    public void setProfissao(String profissao) {
+        this.profissao = profissao;
+    }
+
+    @Override
+    public String toString() {
+        return "Nome: " + nome + ", Email: " + email + ", Profissão: " + profissao;
+    }
 }

16f900f461340edc5e1a29130a5575070652134e
Nagel <130867271+Nagel-P@users.noreply.github.com>
2024-04-16 21:09:12 -0300

    Update Sistema.java

--- Sistema.java
+++ Sistema.java
@@ -33,3 +33,15 @@ class Sistema {
             }
         } while (op != 0);
     }
+    
+    private static void menu() {
+        System.out.println("\n===== Menu =====\n");
+        System.out.println("(1) Cadastrar");
+        System.out.println("(2) Buscar");
+        System.out.println("(3) Editar");
+        System.out.println("(4) Excluir");
+        System.out.println("(5) Listar");
+        System.out.println("(6) Excluir Todos");
+        System.out.println("(0) Sair");
+        System.out.print("Opção: ");
+    }
